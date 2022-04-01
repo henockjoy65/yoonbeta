9946cb977ab7b081930aa5f827369a85e8f3e28b
henockjoy65 <89825644+henockjoy65@users.noreply.github.com>
2022-04-01 07:57:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/deleterobot.git /deleterobot
+  git clone https://github.com/henockjoy65/yoonbeta.git /yoonbeta
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /deleterobot
+  git clone $UPSTREAM_REPO /yoonbeta
 fi
-cd /deleterobot
+cd /yoonbeta
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
