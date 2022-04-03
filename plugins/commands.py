53194227698643b968372064bc55450d558ed8aa
henockjoy65 <89825644+henockjoy65@users.noreply.github.com>
2022-04-03 16:02:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,7 @@ async def start(client, message: pyrogram.types.Message):
             await db.add_chat(message.chat.id, message.chat.title)
         return 
     
-    if not await db.is_user_exist(message.from_user.id):
+    if not await db.is_user_exist(message.from_user.id)
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     
